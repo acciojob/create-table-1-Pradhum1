ae6f00ce6baa0b46fb347ae843103c1fc1db1b09
Pradhum1 <113616448+Pradhum1@users.noreply.github.com>
2024-08-13 14:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function insert_Row() {
     //Write your code here
 	let table = document.getElementById('sampleTable');
-	let btn = document.querySelector('input');
-	btn.addEventListener("click", addRow);
+	//let btn = document.querySelector('input');
+	//btn.addEventListener("click", addRow);
 
-	function addRow(){
+	//function addRow(){
 		let row = table.insertRow(0);
 		row.insertCell(0).innerHTML = "New Cell1";
 		row.insertCell(1).innerHTML = "New Cell2";
-	}
+	
 
 	
   
